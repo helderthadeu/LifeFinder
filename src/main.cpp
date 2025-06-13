a3388ec93c440ad9fcf9f1e19460b957da7d2fa0
helderthadeu <helderthadeubh@gmail.com>
2025-06-13 18:03:16 -0300

    chaging strategic

--- src/main.cpp
+++ src/main.cpp
@@ -1,164 +1,190 @@
-// #include <Arduino.h>
-// #include <Adafruit_MLX90614.h>
-// #include <Wire.h>
-// #include <ESP32Servo.h>
+#include <Arduino.h>
+#include "DHT.h"
+#include <queue>
+#include <Servo.h>
+#include <Ultrasonic.h>
 
-// #define RXD2 16
-// #define TXD2 17
+#define trigPin 11
+#define echoPin 10
 
-// #define servoPin 15
+#define OT1_PIN 32
+#define OT2_PIN 33
 
-// struct Presence_sensor
-// {
-//   float distance;
-//   bool presence;
-// };
+#define DHTPIN 13
 
-// QueueHandle_t presence_queue;
-// QueueHandle_t temperature_queue;
+#define SERVO_PIN 12
 
-// SemaphoreHandle_t start_close_search;
-// SemaphoreHandle_t activate_secondary_sensor;
+using namespace std;
 
-// SemaphoreHandle_t serial_mutex = xSemaphoreCreateMutex();
+struct Presence_sensor
+{
+    float distance;
+    bool presence;
+};
 
-// void process_data(void *parameters)
-// {
-//   unsigned long last_time_checked = 0;
-//   bool start_search = false;
-//   float temperature = 0.0;
-//   Presence_sensor sensor_data;
-//   for (;;)
-//   {
-//     xQueueReceive(presence_queue, &sensor_data, portMAX_DELAY);
-//     if (!start_search)
-//     {
-//       start_search = (sensor_data.presence && sensor_data.distance <= 3.0);
-//     }
-//     if (start_search)
-//     {
-//       xSemaphoreGive(activate_secondary_sensor);
+QueueHandle_t presence_queue;
+QueueHandle_t humidity_queue;
+QueueHandle_t distance_queue;
+QueueHandle_t servo_angle_queue;
 
-//       xQueueReceive(temperature_queue, &temperature, portMAX_DELAY);
+SemaphoreHandle_t start_scan;
+SemaphoreHandle_t start_distance_measurement;
 
-//       if (temperature >= 33)
-//       {
-//         xSemaphoreTake(serial_mutex, portMAX_DELAY);
-//         Serial.println("Probably there is a person nearby.");
-//         xSemaphoreGive(serial_mutex);
-//       }
-//       if (millis() - last_time_checked >= 10000)
-//       {
-//         xSemaphoreTake(serial_mutex, portMAX_DELAY);
-//         Serial.print("Presence detected: ");
-//         Serial.print(sensor_data.presence);
-//         Serial.print(", Distance: ");
-//         Serial.print(sensor_data.distance);
-//         Serial.println(" m");
-//         last_time_checked = millis();
-//         start_search = false; // Reset the search flag after processing
-//         xSemaphoreGive(serial_mutex);
-//       }
-//     }
-//     else
-//     {
-//       last_time_checked = millis();
-//     }
-//   }
-// }
+SemaphoreHandle_t serial_mutex = xSemaphoreCreateMutex();
 
-// void scan_presence(void *parameters)
-// {
-//   Presence_sensor sensor_data;
-//   for (;;)
-//   {
-//     if (Serial2.available())
-//     {
-//       uint8_t header = Serial2.read();
+void process_data(void *parameters)
+{
+    unsigned long last_time_checked = 0;
 
-//       // Verifica se é o início de um pacote (padrão: 0xAA 0x00 ...)
-//       if (header == 0xAA && Serial2.peek() == 0x00)
-//       {
-//         vTaskDelay(100 / portTICK_PERIOD_MS);
-//         uint8_t packet[20];
-//         packet[0] = header;
-//         for (int i = 1; i < 20 && Serial2.available(); i++)
-//         {
-//           packet[i] = Serial2.read();
-//         }
+    float humidity = 0.0;
+    Presence_sensor sensor_data;
+    int count_average = 10;
+    int total_humidity = 0;
+    queue<int> humidity_values;
 
-//         // Presença detectada?
-//         bool presence = packet[8] > 0;
-//         uint16_t distance = packet[9]; // distância em decímetros (~10 cm)
+    for (;;)
+    {
+        xSemaphoreTake(start_scan, portMAX_DELAY);
+        xSemaphoreGive(start_scan);
+        xSemaphoreTake(start_distance_measurement, portMAX_DELAY);
+        xSemaphoreGive(start_distance_measurement);
+        xQueueReceive(presence_queue, &sensor_data, portMAX_DELAY);
+        xQueueReceive(humidity_queue, &humidity, portMAX_DELAY);
 
-//         float distance_m = distance / 10.0;
+        humidity_values.push(humidity);
+        total_humidity += humidity;
 
-//         sensor_data.presence = presence;
-//         sensor_data.distance = distance_m;
-//         xQueueSend(presence_queue, &sensor_data, 0);
-//       }
-//     }
-//     vTaskDelay(1000 / portTICK_PERIOD_MS); // Espera 1 segundo antes de verificar novamente
-//   }
-// }
+        if (count_average > 1)
+        {
+            if (humidity * 1.1 > (total_humidity / 10) && humidity * 0.9 < (total_humidity / 10))
+            {
+                xSemaphoreTake(serial_mutex, portMAX_DELAY);
+                Serial.println("There is a unexpected humidity change!");
+                xSemaphoreGive(serial_mutex);
+                
+                xSemaphoreGive(start_scan);
+                xSemaphoreTake(start_scan, portMAX_DELAY);
+                xSemaphoreGive(start_distance_measurement);
+                xSemaphoreTake(start_distance_measurement, portMAX_DELAY);
+            }
 
-// void scan_temperature(void *mlx_parameter)
-// {
-//   Adafruit_MLX90614 *mlx = (Adafruit_MLX90614 *)mlx_parameter;
-//   for (;;)
-//   {
+            total_humidity -= humidity_values.front();
+            humidity_values.pop();
+        }
+        else
+        {
+            count_average--;
+        }
+    }
+}
 
-//     if (xSemaphoreTake(activate_secondary_sensor, portMAX_DELAY) == pdTRUE)
-//     {
-//       float temperature = mlx->readObjectTempC();
-//       xQueueSend(temperature_queue, &temperature, 0);
-//     }
-//     vTaskDelay(1000 / portTICK_PERIOD_MS);
-//   }
-// }
+void measure_distance(void *ultrassonic_parameters)
+{
+    Ultrasonic *ultrasonic = (Ultrasonic *)ultrassonic_parameters;
+    for (;;)
+    {
+        xSemaphoreTake(start_distance_measurement, portMAX_DELAY);
+        float distance = ultrasonic->read();
+        xQueueSend(distance_queue, &distance, 0);
+        
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+        xSemaphoreGive(start_distance_measurement);
+    }
+}
 
-// void servo_controller_task(void *controller)
-// {
-//   Servo *servo = (Servo *)controller;
+void scan_presence(void *parameters)
+{
+    Presence_sensor sensor_data;
+    for (;;)
+    {
+        if (Serial2.available())
+        {
+            uint8_t header = Serial2.read();
 
-//   servo->write(0);
-//   vTaskDelay(1000 / portTICK_PERIOD_MS);
+            // Verifica se é o início de um pacote (padrão: 0xAA 0x00 ...)
+            if (header == 0xAA && Serial2.peek() == 0x00)
+            {
+                vTaskDelay(100 / portTICK_PERIOD_MS);
+                uint8_t packet[20];
+                packet[0] = header;
+                for (int i = 1; i < 20 && Serial2.available(); i++)
+                {
+                    packet[i] = Serial2.read();
+                }
 
-//   for (int posDegrees = 0; posDegrees <= 180; posDegrees++)
-//   {
-//     servo->write(posDegrees);
-//     vTaskDelay(50 / portTICK_PERIOD_MS);
-//   }
-// }
+                // Presença detectada?
+                bool presence = packet[8] > 0;
+                uint16_t distance = packet[9]; // distância em decímetros (~10 cm)
 
-// void setup()
-// {
-//   Serial.begin(9600);
-//   Serial2.begin(256000, SERIAL_8N1, RXD2, TXD2);
+                float distance_m = distance / 10.0;
 
-//   start_close_search = xSemaphoreCreateBinary();
-//   activate_secondary_sensor = xSemaphoreCreateBinary();
+                sensor_data.presence = presence;
+                sensor_data.distance = distance_m;
+                xQueueSend(presence_queue, &sensor_data, 0);
+            }
+        }
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Espera 1 segundo antes de verificar novamente
+    }
+}
 
-//   Adafruit_MLX90614 mlx = Adafruit_MLX90614();
-//   // Wire.begin(21, 22);
-//   while (!mlx.begin())
-//   {
-//     Serial.println("Error connecting to MLX sensor. Check wiring.");
-//     delay(1000);
-//     // while (1)
-//     //   ;
-//   };
+void scan_humidity(void *dht_parameters)
+{
+    DHT *dht = (DHT *)dht_parameters;
+    float humidity = 0;
+    for (;;)
+    {
+        humidity = dht->readHumidity();
+        if (isnan(humidity))
+        {
+            xSemaphoreTake(serial_mutex, portMAX_DELAY);
+            Serial.println("Failed to read humidity from DHT sensor!");
+            xSemaphoreGive(serial_mutex);
+        }
+        else
+        {
+            xQueueSend(humidity_queue, &humidity, 0);
+        }
+    }
+}
 
-//   Servo servo_controller;
-//   servo_controller.write(0);
+void servo_controller_task(void *controller)
+{
+    Servo *servo = (Servo *)controller;
 
-//   servo_controller.attach(servoPin);
+    xSemaphoreTake(start_scan, portMAX_DELAY);
 
-//   xTaskCreate(scan_presence, "ScanPresence", 2048, NULL, 2, NULL);
-//   xTaskCreate(scan_temperature, "ScanTemperature", 2048, (void *)&mlx, 1, NULL);
-//   xTaskCreate(process_data, "ProcessData", 2048, NULL, 1, NULL);
-// }
+    for (int posDegrees = 0; posDegrees <= 180; posDegrees++)
+    {
+        xQueueSend(servo_angle_queue, &posDegrees, 0);
+        servo->write(posDegrees);
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+    }
+    xSemaphoreGive(start_scan);
+}
 
-// void loop()
-// {
-// }
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(OT1_PIN, INPUT_PULLDOWN);
+    pinMode(OT2_PIN, INPUT_PULLDOWN);
+
+    DHT dht(DHTPIN, DHT11);
+    Ultrasonic ultrasonic(trigPin, echoPin);
+    Servo servo;
+    servo.attach(SERVO_PIN);
+    servo.write(0);
+
+    start_scan = xSemaphoreCreateBinary();
+    activate_secondary_sensor = xSemaphoreCreateBinary();
+
+    xTaskCreate(measure_distance, "MeasureDistance", 2048, (void *)&ultrasonic, 1, NULL);
+    xTaskCreate(scan_presence, "ScanPresence", 2048, NULL, 2, NULL);
+    xTaskCreate(scan_humidity, "ScanHumidity", 2048, (void *)&dht, 1, NULL);
+    xTaskCreate(process_data, "ProcessData", 2048, NULL, 1, NULL);
+    xTaskCreate(servo_controller_task, "ServoController", 2048, (void *)&servo, 1, NULL);
+}
+
+void loop()
+{
+}
